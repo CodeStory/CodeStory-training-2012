6c3b76ef39b3cc9311ae9efbf2c1be413bc86b0e
David Gageot <david@gageot.net>
2012-04-04 18:44:42 +0200

    Refactoring

--- src/main/java/CodeStoryServer.java
+++ src/main/java/CodeStoryServer.java
@@ -1,8 +1,12 @@
 import com.sun.jersey.api.container.httpserver.*;
 import com.sun.net.httpserver.*;
+import net.gageot.codestory.*;
 
 import javax.ws.rs.*;
 import java.io.*;
+import java.util.*;
+
+import static java.util.Arrays.*;
 
 @Path("/")
 public class CodeStoryServer {
@@ -12,15 +16,21 @@ public class CodeStoryServer {
 	}
 
 	@GET
-	@Path("mustache.js")
-	public File mustache() {
-		return new File("mustache.js");
+	@Path("commits")
+	@Produces("application/json;charset=UTF-8")
+	public List<Commit> commits() {
+		return asList( //
+				new Commit("dgageot", "02/12/2012", "Troisième commit", "https://secure.gravatar.com/avatar/f0887bf6175ba40dca795eb37883a8cf"), //
+				new Commit("jeanlaurent", "01/12/2012", "Deuxième commit", "https://secure.gravatar.com/avatar/649d3668d3ba68e75a3441dec9eac26e"), //
+				new Commit("eric", "29/11/2012", "Premier commit", "https://secure.gravatar.com/avatar/77da98419ae312eb0e322a3dac44a734") //
+		);
 	}
 
 	@GET
-	@Path("jquery.js")
-	public File jquery() {
-		return new File("jquery.js");
+	@Path("{path: .*\\.js}")
+	@Produces("text/javascript")
+	public File js(@PathParam("path") String path) {
+		return new File(path);
 	}
 
 	public static void main(String[] args) throws Exception {
---
+++ src/main/java/net/gageot/codestory/Commit.java
@@ -0,0 +1,12 @@
+package net.gageot.codestory;
+
+import lombok.*;
+
+@Data
+public class Commit {
+	private final String login;
+	private final String date;
+	private final String message;
+	private final String avatarUrl;
+
+}
