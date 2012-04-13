8c8ff05fc892c24098078d6b6f4fbb7505eee6e7
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2012-04-13 12:24:55 +0200

    Introduce fake badges

--- src/test/resources/HomePageTest.js
+++ src/test/resources/HomePageTest.js
@@ -3,7 +3,7 @@ var assert = require("assert");
 
 port = process.argv[2];
 
-Browser.visit("http://localhost:" + port + "/", function (e, browser) {
+Browser.visit("http://localhost:" + port + "/", function (e, browser, status) {
     assert.equal(browser.text("title"), "CodeStory - HomePage");
 
     assert.ok(browser.query("#commits :nth-child(1):contains('Quatrieme commit')"));
@@ -15,4 +15,8 @@ Browser.visit("http://localhost:" + port + "/", function (e, browser) {
     assert.ok(browser.query("#commits :nth-child(2):contains('03/01/2012')"));
     assert.ok(browser.query("#commits :nth-child(3):contains('02/01/2012')"));
     assert.ok(browser.query("#commits :nth-child(4):contains('01/01/2012')"));
+});
+
+Browser.visit("http://localhost:" + port + "/", function (e, browser, status) {
+    assert.ok(browser.query("#best-commiter:contains('jeanlaurent')"));
 });
