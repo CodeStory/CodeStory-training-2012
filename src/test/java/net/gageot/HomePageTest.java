aafc25fedc8e5f1f091db32c78b52cfbb4598b24
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2012-04-13 11:13:11 +0200

    moved bash commits to badges and vice versa in the ui

--- src/test/java/net/gageot/HomePageTest.java
+++ src/test/java/net/gageot/HomePageTest.java
@@ -29,7 +29,7 @@ public class HomePageTest {
 				new Commit("dgageot", "03/01/2012", "Quatrieme commit", "https://secure.gravatar.com/avatar/f0887bf6175ba40dca795eb37883a8cf"), //
 				new Commit("jeanlaurent", "03/01/2012", "Troisieme commit", "https://secure.gravatar.com/avatar/649d3668d3ba68e75a3441dec9eac26e"), //
 				new Commit("jeanlaurent", "02/01/2012", "Deuxieme commit", "https://secure.gravatar.com/avatar/649d3668d3ba68e75a3441dec9eac26e"), //
-				new Commit("eric", "01/01/2012", "Premier commit", "https://secure.gravatar.com/avatar/77da98419ae312eb0e322a3dac44a734"));
+				new Commit("eric", "01/01/2012", "Premier commit", "https://secure.gravatar.com/avatar/77da98419ae312eb0e322a3dac44a734")));
 
 		int exitCode = runZombieJsTest("HomePageTest.js");
 
