a938445abbf77e3b5bf14e584bfaf140d92f5c1c
David Gageot <david@gageot.net>
2012-04-13 16:07:39 +0200

    Suppress output

--- mochalauncher.sh
+++ mochalauncher.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 export NODE_PATH=/usr/local/lib/node_modules
 export PATH=$PATH:/usr/local/bin
 
-mocha --ui qunit --compilers coffee:coffee-script $1
+mocha --ui qunit --compilers coffee:coffee-script $1 > /dev/null 2>&1
