357b0feb724e3b98beb6776e15d87e5c7e123dca
David Gageot <david@gageot.net>
2012-04-13 10:32:42 +0200

    Fix json template

--- src/main/java/net/gageot/CodeStoryResource.java
+++ src/main/java/net/gageot/CodeStoryResource.java
@@ -55,9 +55,9 @@ public class CodeStoryResource {
 	@Produces("application/json;charset=UTF-8")
 	public List<Commit> commits() {
 		return asList( //
-				new Commit("https://secure.gravatar.com/avatar/f0887bf6175ba40dca795eb37883a8cf", "dgageot", "Troisième commit", "03/01/2012"), //
-				new Commit("https://secure.gravatar.com/avatar/649d3668d3ba68e75a3441dec9eac26e", "jeanlaurent", "Deuxième commit", "02/01/2012"), //
-				new Commit("https://secure.gravatar.com/avatar/77da98419ae312eb0e322a3dac44a734", "eric", "Premier commit", "01/01/2012"));
+				new Commit("dgageot", "03/01/2012", "Troisieme commit", "https://secure.gravatar.com/avatar/f0887bf6175ba40dca795eb37883a8cf"), //
+				new Commit("jeanlaurent", "02/01/2012", "Deuxieme commit", "https://secure.gravatar.com/avatar/649d3668d3ba68e75a3441dec9eac26e"), //
+				new Commit("eric", "01/01/2012", "Premier commit", "https://secure.gravatar.com/avatar/77da98419ae312eb0e322a3dac44a734"));
 	}
 
 	private static File staticResource(String path) {
--- src/test/resources/HomePageTest.js
+++ src/test/resources/HomePageTest.js
@@ -6,11 +6,11 @@ port = process.argv[2];
 Browser.visit("http://localhost:" + port + "/", function (e, browser) {
     assert.equal(browser.text("title"), "CodeStory - HomePage");
 
-    assert.ok(browser.query("#commits :nth-child(1):contains('Premier commit')"));
+    assert.ok(browser.query("#commits :nth-child(1):contains('Troisieme commit')"));
     assert.ok(browser.query("#commits :nth-child(2):contains('Deuxieme commit')"));
-    assert.ok(browser.query("#commits :nth-child(3):contains('Troisieme commit')"));
+    assert.ok(browser.query("#commits :nth-child(3):contains('Premier commit')"));
 
-    assert.ok(browser.query("#commits :nth-child(1):contains('26/01/2012')"));
-    assert.ok(browser.query("#commits :nth-child(2):contains('24/01/2012')"));
-    assert.ok(browser.query("#commits :nth-child(3):contains('22/01/2012')"));
+    assert.ok(browser.query("#commits :nth-child(1):contains('03/01/2012')"));
+    assert.ok(browser.query("#commits :nth-child(2):contains('02/01/2012')"));
+    assert.ok(browser.query("#commits :nth-child(3):contains('01/01/2012')"));
 });
