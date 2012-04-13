5b246805530c56837fd54f05309c30fb1c84e4bc
David Gageot <david@gageot.net>
2012-04-13 18:14:34 +0200

    Top committer in Groovy

--- src/main/java/net/gageot/AllBadges.java
+++ src/main/java/net/gageot/AllBadges.java
@@ -2,12 +2,13 @@ package net.gageot;
 
 import com.google.common.util.concurrent.*;
 import com.google.inject.*;
+import groovy.lang.Binding;
+import groovy.lang.*;
 import net.gageot.codestory.*;
 
 import java.util.*;
 
 public class AllBadges {
-
 	private AllCommits allCommits;
 
 	@Inject
@@ -36,5 +37,17 @@ public class AllBadges {
 			}
 		}
 		return topCommiter;
+	}
+
+	public String getTopCommiterGroovy() {
+		Binding binding = new Binding();
+		binding.setVariable("commits", allCommits.list());
+		GroovyShell shell = new GroovyShell(binding);
+
+		try {
+			return (String) shell.evaluate("((List<net.gageot.codestory.Commit>) commits).get(0).getLogin();");
+		} catch (Exception e) {
+			throw new RuntimeException("unable to understand the groovy script", e);
+		}
 	}
 }
--- src/test/java/net/gageot/AllBadgesTest.java
+++ src/test/java/net/gageot/AllBadgesTest.java
@@ -2,27 +2,41 @@ package net.gageot;
 
 import net.gageot.codestory.*;
 import org.junit.*;
+import org.mockito.*;
 
 import java.util.*;
 
+import static java.util.Arrays.*;
 import static org.fest.assertions.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class AllBadgesTest {
+	private AllCommits allCommits = Mockito.mock(AllCommits.class);
+	private AllBadges allBadges = new AllBadges(allCommits);
 
 	@Test
 	public void shouldFindTheTopCommiter() {
-		AllCommits allCommits = mock(AllCommits.class);
 		when(allCommits.list()).thenReturn(commitList());
-		String name = new AllBadges(allCommits).topcommiter();
+
+		String name = allBadges.topcommiter();
+
 		assertThat(name).isEqualTo("jeanlaurent");
 	}
 
-	private List<Commit> commitList() {
-		return Arrays.asList(commitFrom("jeanlaurent"), commitFrom("jeanlaurent"), commitFrom("David"), commitFrom("Eric"));
+	@Test
+	public void should_get_top_commiter() {
+		when(allCommits.list()).thenReturn(commitList());
+
+		String name = allBadges.getTopCommiterGroovy();
+
+		assertThat(name).isEqualTo("jeanlaurent");
 	}
 
-	private Commit commitFrom(String commiter) {
+	private static List<Commit> commitList() {
+		return asList(commitFrom("jeanlaurent"), commitFrom("jeanlaurent"), commitFrom("David"), commitFrom("Eric"));
+	}
+
+	private static Commit commitFrom(String commiter) {
 		return new Commit(commiter, "", "", "");
 	}
 }
