356eaa0e48d19f2625d9d29580b1529b7d176223
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2012-04-17 17:06:33 +0200

    Adding Badge for Eric

---
+++ src/main/java/net/gageot/codestory/Badge.java
@@ -0,0 +1,12 @@
+package net.gageot.codestory;
+
+import lombok.*;
+
+@Data
+public class Badge {
+	private final String date;
+	private final String name;
+	private final String login;
+	private final String avatarUrl;
+
+}
